27e674c4eadb76ff2e6ce32814181f107049c518
istasi <istasi@gmail.com>
2014-08-10 20:21:14 +0200

    just more testing

--- programs/justtesting.lua
+++ programs/justtesting.lua
@@ -1,2 +1,13 @@
-_G = nil
-system.event:destroy ();
+system.desktop:create ('test', true):attr ({
+	['position'] = 'relative',
+	['x'] = 30,
+	['y'] = 30,
+
+	['width'] = 20,
+	['height'] = 3,
+
+	['align'] = 'center',
+	['vertical-align'] = 'center',
+
+	['background-color'] = 0x990000,
+}):draw ();
